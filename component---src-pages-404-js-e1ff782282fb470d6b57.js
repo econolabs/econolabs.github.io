7d033f8d9ceb8da7fce8c58916e0b42ed2628baf
Econolabs <da_golovenkin@vyatsu.ru>
2019-07-21 07:17:04 +0000

    Updates

---
+++ component---src-pages-404-js-e1ff782282fb470d6b57.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{185:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(28),o=n(29);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-e1ff782282fb470d6b57.js.map
