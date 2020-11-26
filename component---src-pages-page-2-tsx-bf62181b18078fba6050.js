e3cbe7fdb92d963b7f64edd9c96684ddcbe43b28
econolabs <da_golovenkin@vyatsu.ru>
2020-11-26 13:25:21 +0000

    Updates

--- component---src-pages-page-2-tsx-bf62181b18078fba6050.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{"+xhR":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Wbzz"),c=a("Bl7J"),r=a("vrFN");t.default=function(e){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2 (",e.path,")"),l.a.createElement(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-tsx-bf62181b18078fba6050.js.map
