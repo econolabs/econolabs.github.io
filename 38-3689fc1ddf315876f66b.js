c2b6cb8579d28b987cf2c7e00355d2626627a164
econolabs <da_golovenkin@vyatsu.ru>
2021-07-08 07:52:02 +0000

    Updates

---
+++ 38-3689fc1ddf315876f66b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[38],{"7/bz":function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return f}));var o=e("q1tI"),c=e.n(o),a=e("/MKj"),i=e("opnR"),u=e("AEIT"),r=e.n(u),d=e("+z1p"),p=e.n(d);function f(n){var t=Object(a.c)(),e=Object(a.d)(i.c).ckeditor_node_content;return c.a.createElement(r.a,{editor:p.a,data:e||"<p>Комментарий</p>",onInit:function(n){},onChange:function(n,e){t(Object(i.d)(e.getData()))},onFocus:function(n,t){}})}}}]);
+//# sourceMappingURL=38-3689fc1ddf315876f66b.js.map
