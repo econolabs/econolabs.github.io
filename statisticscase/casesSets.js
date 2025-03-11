be0175426a1c97b7f3fce4331e1f418935eeb221
DMGL <da_golovenkin@vyatsu.ru>
2025-03-11 15:16:04 +0300

    statisticscase
    statisticscase updated

--- statisticscase/casesSets.js
+++ statisticscase/casesSets.js
@@ -96,11 +96,11 @@ let casesSets = [
 `},
 
 
-{
-   id: 8,
-   formula: "QUARTILE",
-   title: "Показатели распределения. КВАРТИЛЬ",
-   html: `
+   {
+      id: 8,
+      formula: "QUARTILE",
+      title: "Показатели распределения. КВАРТИЛЬ",
+      html: `
        <div class="alert alert-primary" role="alert">
    Введите в колонку заголовок и 11 цифр. Рассчитайте для них первый квартиль 
        </div>
@@ -117,7 +117,7 @@ let casesSets = [
     `},
 
 
-    {
+   {
       id: 9,
       formula: "PERCENTILE",
       title: "Показатели распределения. ПЕРСЕНТИЛЬ",
@@ -129,8 +129,7 @@ let casesSets = [
          PERCENTILE function Returns the k-th percentile of values in a range. You can use this function to establish a threshold of acceptance. For example, you can decide to examine candidates who score above the 90th percentile.<br>
          =PERCENTILE(array,k) k Required. The percentile value in the range 0..1, inclusive
          <hr>
-       Функция КВАРТИЛЬ озвращает квартиль множества данных. Квартиль часто используются при анализе продаж для разбиения генеральной совокупности на группы. Например, можно воспользоваться функцией КВАРТИЛЬ, чтобы найти среди всех предприятий 25 процентов наиболее доходных.<br>
-       =Функция ПЕРСЕНТИЛЬ ПРОЦЕНТИЛЬ.ИСКЛ и Функция ПРОЦЕНТИЛЬ.ВКЛ.<br>
+        =Функция ПЕРСЕНТИЛЬ ПРОЦЕНТИЛЬ.ИСКЛ и Функция ПРОЦЕНТИЛЬ.ВКЛ.<br>
        ПЕРСЕНТИЛЬ(массив;k) k  Обязательный. Значение процентили в интервале от 0 до 1, включая эти числа.
           </div>   
        `},
@@ -144,7 +143,7 @@ let casesSets = [
     Введите в колонку заголовок и 11 цифр. Рассчитайте для них среднее абсолютных значений отклонений точек данных от среднего 
         </div>
         <div>
-     Returns the average of the absolute deviations of data points from their mean.<br><hr>
+     AVEDEV Returns the average of the absolute deviations of data points from their mean.<br><hr>
      Возвращает среднее абсолютных значений отклонений точек данных от среднего. СРОТКЛ является мерой разброса множества данных.<br>
      Уравнение для среднего отклонения<br>
      <img src='https://cxcs.microsoft.net/static/public/office/ru-ru/2c328fff-b4af-4e42-bb34-4f7bf5e2e85c/ee16ba75f4d39db65ad8550c92983268af136c0b.gif' />
@@ -158,7 +157,11 @@ let casesSets = [
          <div class="alert alert-primary" role="alert">
     Введите в колонку заголовок и 11 цифр. Рассчитайте для них сумму квадратов отклонений точек данных от их среднего 
         </div>     
-     `},
+     `,
+      hint: `<div>
+               DEVSQ
+            </div>`
+   },
 
 
 
@@ -170,105 +173,130 @@ let casesSets = [
     Введите в две  колонки заголовок и по 11 цифр. Во второй колонке все цифры ВДВОЕ больше.<br>
     Рассчитайте для них коэффициент корреляции двух диапазонов ячеек <br>
     Сделайте вывод
-        </div>     
-     `},
+        </div>  `
+      ,
+      hint: `<div>
+               CORREL
+            </div>`
+   },
 
    {
       id: 13, formula: "CORREL",
       title: "Коэффициент корреляции двух диапазонов ячеек 2",
       html: `
-         <div class="alert alert-primary" role="alert">
-    Введите в две колонки заголовок и по 11 цифр. В первой колонке числа увеличиваются, во второй уменьшаются.<br>
-    Рассчитайте для них коэффициент корреляции двух диапазонов ячеек <br>
+         < div class= "alert alert-primary" role="alert" >
+         Введите в две колонки заголовок и по 11 цифр.В первой колонке числа увеличиваются, во второй уменьшаются.< br >
+            Рассчитайте для них коэффициент корреляции двух диапазонов ячеек<br>
     Сделайте вывод
-        </div>     
-     `},
+        </div>
+   `,
+      hint: `<div>
+            CORREL
+         </div>`},
 
 
    {
       id: 14, formula: "CORREL",
       title: "Коэффициент корреляции двух диапазонов ячеек 3",
       html: `
-         <div class="alert alert-primary" role="alert">
-    Введите в две колонки заголовок и по 11 цифр. Все цифры рандомные.<br>
-    Рассчитайте для них коэффициент корреляции двух диапазонов ячеек <br>
-    Сделайте вывод
-        </div>     
-     `},
+   < div class="alert alert-primary" role = "alert" >
+      Введите в две колонки заголовок и по 11 цифр.Все цифры рандомные.< br >
+         Рассчитайте для них коэффициент корреляции двух диапазонов ячеек < br >
+            Сделайте вывод
+        </div >
+   `,
+      hint: `<div>
+            CORREL
+         </div>`},
 
 
    {
       id: 15, formula: "COVARIANCE.P",
       title: "Ковариация (среднее произведений отклонений для каждой пары точек в двух наборах данных) 1",
       html: `
-         <div class="alert alert-primary" role="alert">
-    Введите в две  колонки заголовок и по 11 цифр. Во второй колонке все цифры ВДВОЕ больше.<br>
-    Рассчитайте для них ковариацию двух диапазонов ячеек<br>
-    Сделайте вывод
-        </div>     
-     `},
+   < div class="alert alert-primary" role = "alert" >
+      Введите в две  колонки заголовок и по 11 цифр.Во второй колонке все цифры ВДВОЕ больше.< br >
+         Рассчитайте для них ковариацию двух диапазонов ячеек < br >
+            Сделайте вывод
+        </div>
+   `,
+      hint: `<div>
+            COVARIANCE.P
+         </div>`},
 
    {
       id: 16, formula: "COVARIANCE.P",
       title: "Ковариация (среднее произведений отклонений для каждой пары точек в двух наборах данных) 2",
       html: `
-         <div class="alert alert-primary" role="alert">
-    Введите в две колонки заголовок и по 11 цифр. В первой колонке числа увеличиваются, во второй уменьшаются.<br>
-   Рассчитайте для них ковариацию двух диапазонов ячеек<br>
-    Сделайте вывод
-        </div>     
-     `},
+   < div class="alert alert-primary" role = "alert" >
+      Введите в две колонки заголовок и по 11 цифр.В первой колонке числа увеличиваются, во второй уменьшаются.< br >
+         Рассчитайте для них ковариацию двух диапазонов ячеек < br >
+            Сделайте вывод
+        </div >
+   `,
+      hint: `<div>
+            COVARIANCE.P
+         </div>`},
 
 
    {
       id: 17, formula: "COVARIANCE.P",
       title: "Ковариация (среднее произведений отклонений для каждой пары точек в двух наборах данных) 3",
       html: `
-         <div class="alert alert-primary" role="alert">
-    Введите в две колонки заголовок и по 11 цифр. Все цифры рандомные.<br>
-    Рассчитайте для них ковариацию двух диапазонов ячеек<br>
-    Сделайте вывод
-        </div>     
-     `},
+   < div class="alert alert-primary" role = "alert" >
+      Введите в две колонки заголовок и по 11 цифр.Все цифры рандомные.< br >
+         Рассчитайте для них ковариацию двух диапазонов ячеек < br >
+            Сделайте вывод
+        </div >
+   `,
+      hint: `<div>
+            COVARIANCE.P
+         </div>`},
 
 
    {
-      id: 18, formula: "SKEW",
-      title: "наклон линии линейной регрессии",
+      id: 18, formula: "SLOPE",
+      title: "Наклон линии линейной регрессии",
       html: `
-           <div class="alert alert-primary" role="alert">
-      Введите в две колонки заголовок и по 11 цифр. Все цифры рандомные.<br>
-      Рассчитайте для них НАКЛОН<br>
-      Возвращает наклон линии линейной регрессии для точек данных в аргументах известные_значения_y и известные_значения_x.<br>
-      Наклон определяется как частное от деления расстояния по вертикали на расстояние по горизонтали между двумя любыми точками прямой; иными словами, наклон — это скорость изменения значений вдоль прямой.
-       SKEW function. Returns the skewness of a distribution.<br>
-       Skewness characterizes the degree of asymmetry of a distribution around its mean.<br>
-       Positive skewness indicates a distribution with an asymmetric tail extending toward more positive values.<br>
-        Negative skewness indicates a distribution with an asymmetric tail extending toward more negative values.<br>
-      Функция НАКЛОН. Возвращает наклон линии линейной регрессии для точек данных в аргументах известные_значения_y и известные_значения_x.<br>
-      Наклон определяется как частное от деления расстояния по вертикали на расстояние по горизонтали между двумя любыми точками прямой; иными словами, наклон — это скорость изменения значений вдоль прямой.
+   < div class="alert alert-primary" role = "alert" >
+      Введите в две колонки заголовок и по 11 цифр..Во второй колонке все цифры ВДВОЕ больше.< br >
+         Рассчитайте для них НАКЛОН < br >
+            Возвращает наклон линии линейной регрессии для точек данных в аргументах известные_значения_y и известные_значения_x.< br >
+               Наклон определяется как частное от деления расстояния по вертикали на расстояние по горизонтали между двумя любыми точками прямой; иными словами, наклон — это скорость изменения значений вдоль прямой.
+       SKEW function. Returns the skewness of a distribution.< br >
+   Skewness characterizes the degree of asymmetry of a distribution around its mean.< br >
+      Positive skewness indicates a distribution with an asymmetric tail extending toward more positive values.< br >
+         Negative skewness indicates a distribution with an asymmetric tail extending toward more negative values.< br >
+            Функция НАКЛОН.Возвращает наклон линии линейной регрессии для точек данных в аргументах известные_значения_y и известные_значения_x.< br >
+               Наклон определяется как частное от деления расстояния по вертикали на расстояние по горизонтали между двумя любыми точками прямой; иными словами, наклон — это скорость изменения значений вдоль прямой.
 
 
-          </div>     
-       `},
+          </div >
+   `,
+   hint: `<div>
+         SLOPE
+      </div>`},
 
 
    {
-      id: 19, formula: "SKEW",
-      title: "наклон линии линейной регрессии",
+      id: 19, formula: "INTERCEPT",
+      title: "ОТРЕЗОК. Точка пересечения линии с осью y",
       html: `
-           <div class="alert alert-primary" role="alert">
-      Введите в две колонки заголовок и по 11 цифр. Все цифры рандомные.<br>
-      Рассчитайте для них Функция ОТРЕЗОК<br>
-      Вычисляет точку пересечения линии с осью y, используя значения аргументов "известные_значения_x" и "известные_значения_y". Точка пересечения находится на оптимальной линии регрессии, проведенной через точки, заданные аргументами "известные_значения_x" и "известные_значения_y". Функция ОТРЕЗОК используется, если нужно определить значение зависимой переменной при нулевом значении независимой переменной. Например, с помощью функции ОТРЕЗОК можно предсказать электрическое сопротивление металла при температуре 0°C, если имеются данные измерений при комнатной температуре и выше.
+   < div class="alert alert-primary" role = "alert" >
+      Введите в две колонки заголовок и по 11 цифр.Все цифры рандомные.< br >
+         Рассчитайте для них Функция ОТРЕЗОК < br >
+            Вычисляет точку пересечения линии с осью y, используя значения аргументов "известные_значения_x" и "известные_значения_y".Точка пересечения находится на оптимальной линии регрессии, проведенной через точки, заданные аргументами "известные_значения_x" и "известные_значения_y".Функция ОТРЕЗОК используется, если нужно определить значение зависимой переменной при нулевом значении независимой переменной.Например, с помощью функции ОТРЕЗОК можно предсказать электрическое сопротивление металла при температуре 0°C, если имеются данные измерений при комнатной температуре и выше.
        INTERCEPT function.<br>
-       Calculates the point at which a line will intersect the y-axis by using existing x-values and y-values. The intercept point is based on a best-fit regression line plotted through the known x-values and known y-values. Use the INTERCEPT function when you want to determine the value of the dependent variable when the independent variable is 0 (zero). For example, you can use the INTERCEPT function to predict a metal's electrical resistance at 0°C when your data points were taken at room temperature and higher<br>
+   Calculates the point at which a line will intersect the y-axis by using existing x-values and y-values. The intercept point is based on a best-fit regression line plotted through the known x-values and known y-values. Use the INTERCEPT function when you want to determine the value of the dependent variable when the independent variable is 0 (zero). For example, you can use the INTERCEPT function to predict a metal's electrical resistance at 0°C when your data points were taken at room temperature and higher<br>
       Функция НАКЛОН. Возвращает наклон линии линейной регрессии для точек данных в аргументах известные_значения_y и известные_значения_x.<br>
-      Наклон определяется как частное от деления расстояния по вертикали на расстояние по горизонтали между двумя любыми точками прямой; иными словами, наклон — это скорость изменения значений вдоль прямой.
+         Наклон определяется как частное от деления расстояния по вертикали на расстояние по горизонтали между двумя любыми точками прямой; иными словами, наклон — это скорость изменения значений вдоль прямой.
 
 
-          </div>     
-       `},
+      </div>     
+       `,
+       hint: `<div>
+             INTERCEPT
+          </div>`},
 
 ]
    ;
